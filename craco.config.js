2234c11bfb830428f7088ffb8a01eced82fe9e30
kalaxiaoke777 <127180457+kalaxiaoke777@users.noreply.github.com>
2025-10-10 22:31:08 +0800

    当前为

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  reactScriptsVersion: "react-scripts",
+  paths: function (paths, env) {
+    paths.appBuild = path.resolve(__dirname, "dist");
+    return paths;
+  },
   webpack: {
     alias: {
       //别名配置
